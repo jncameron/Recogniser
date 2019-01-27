916d041d944391552506c7189d279037f4c2257c
John Cameron <john.n.cameron@gmail.com>
2019-01-27 18:58:57 +1100

    Updates

---
+++ precache-manifest.54f4fd798a1a69ca87064ff5ec073bc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8def0ff0fea8d74a8aef48819c49e43e",
+    "url": "/jncameron/recogniser/static/media/bg.8def0ff0.jpg"
+  },
+  {
+    "revision": "4ad40db8ebb970abffbd",
+    "url": "/jncameron/recogniser/static/js/runtime~main.4ad40db8.js"
+  },
+  {
+    "revision": "c992897afed54318e117",
+    "url": "/jncameron/recogniser/static/js/main.c992897a.chunk.js"
+  },
+  {
+    "revision": "f837e3a776487408bfaa",
+    "url": "/jncameron/recogniser/static/js/1.f837e3a7.chunk.js"
+  },
+  {
+    "revision": "c992897afed54318e117",
+    "url": "/jncameron/recogniser/static/css/main.db7e40f5.chunk.css"
+  },
+  {
+    "revision": "f837e3a776487408bfaa",
+    "url": "/jncameron/recogniser/static/css/1.087507ec.chunk.css"
+  },
+  {
+    "revision": "db967bd917e8db425bbb072441a24dae",
+    "url": "/jncameron/recogniser/index.html"
+  }
+];
